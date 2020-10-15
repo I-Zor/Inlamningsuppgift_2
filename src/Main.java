eab7afc64720051c73b87af149e8e8e91335717a
IvonaZoricic <ivona.zoricic@gmail.com>
2020-10-15 15:45:41 +0200

    Change in ReceptionistUtil

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,34 @@ public class Main {
 
         String originalFile = "src/customers.txt";
         ListUtil lu = new ListUtil();
+        ReceptionistUtil ru = new ReceptionistUtil();
+        TrainerUtil tu = new TrainerUtil();
         lu.readDataFromOriginalFile(originalFile);
 
         Scanner sc = new Scanner(System.in);
-        Mode m = new Mode();
         int respons = 0;
+        boolean right = true;
 
-        while (true) {
+        while (right) {
             System.out.println("Välj arbetssätt (skriv 1 eller 2)" +
                     "\n1 - receptionist\n2 - personliga tränare");
             try {
                 respons = sc.nextInt();
+             right = false;
             } catch (InputMismatchException e) {
                 e.printStackTrace();
                 System.out.println("Felaktig inmatning. Försök igen.");
                 sc.nextLine();
             }
-            if (respons == 1 || respons == 2)
-            m.isChoosingMode(respons);
-            break;
+            if (respons == 1) {
+                System.out.println("Ange namn eller personnummer:");
+                String input = sc.next();
+                System.out.println(ru.isLookingForMembers(input));
+            } else if (respons == 2) {
+                System.out.println("Ange namn eller personnummer:");
+                String input = sc.next();
+                System.out.println(tu.isLookingForTrainingOccasion(input));
+            }
         }
     }
 }
--- src/ReceptionistUtil.java
+++ src/ReceptionistUtil.java
@@ -7,24 +7,25 @@ public class ReceptionistUtil {
    DateUtil du = new DateUtil();
    ListUtil lu = new ListUtil();
    String writeToFile = "trainingOccasions.txt";
+   String readFromFile = "src/customers.txt";
 
 
-   public String isLookingForMembers(String indata) {
-      List<Person> personList = new ArrayList<>();
+   public String isLookingForMembers(String input) {
+      List<Person> personList = lu.readDataFromOriginalFile(readFromFile);
       LocalDate lt = LocalDate.now();
-      String response = "";
+      String response = "blabla";
       for (Person p : personList) {
-         if ((p.name.equalsIgnoreCase(indata) || p.pn.equals(indata)) &&
+         if ((p.name.equalsIgnoreCase(input) || p.pn.equals(input)) &&
                  du.isDurationShorterThanYear(p.dateOfAnnualFee, lt)) {
             response = p.name + " är nuvarande medlem";
             TrainingOccasion to = new TrainingOccasion(p.pn, p.name, lt);
             lu.writeDataToTrainersFile(writeToFile, to);
          }
-         else if ((p.name.equalsIgnoreCase(indata) || p.pn.equals(indata)) &&
+         else if ((p.name.equalsIgnoreCase(input) || p.pn.equals(input)) &&
                  !du.isDurationShorterThanYear(p.dateOfAnnualFee, lt))
             response = p.name + " är förre detta medlem";
          else
-            response = indata + " är obehörig";
+            response = input + " är obehörig";
       }
       return response;
    }
